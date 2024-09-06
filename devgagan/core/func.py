26e9e0295822e4353725e861208443aeecf4f9f0
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-09-06 22:15:52 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -113,8 +113,8 @@ async def progress_bar(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["🌑" for i in range(math.floor(percentage / 10))]),
-            ''.join(["🌕" for i in range(10 - math.floor(percentage / 10))]))
+            ''.join(["⭐" for i in range(math.floor(percentage / 10))]),
+            ''.join(["✩" for i in range(10 - math.floor(percentage / 10))]))
             
         tmp = progress + PROGRESS_BAR.format( 
             round(percentage, 2),
